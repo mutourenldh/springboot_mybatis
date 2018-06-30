a1c65381a08dc6d5ac060f2643485339fdc993bb
lidonghao <mutouren.ldh@qq.com>
2018-06-30 10:58:45 +0800

    a

--- src/main/java/com/haoge/SpringbootMybatisApplication.java
+++ src/main/java/com/haoge/SpringbootMybatisApplication.java
@@ -1,6 +1,5 @@
 package com.haoge;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@MapperScan(value = "com.haoge.mapper")
